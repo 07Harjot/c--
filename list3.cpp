ea7a9373b6cd933a5d38ac977239e1182a711010
07Harjot <singhharjot1970@gmail.com>
2023-03-09 13:55:34 +0530

    commit

--- list3.cpp
+++ list3.cpp
@@ -1,5 +1,40 @@
 #include<iostream>
 using namespace std;
+class node{
+public:
+int data;
+node *next;
+
+node(int data){
+    this->data=data;
+    this->next=NULL;
+}
+};
+void addnodeatleft(node *&head,int data){
+    node *temp=new node(data);
+    temp->data=data;
+    temp->next=head;
+    head=temp;
+}
+
+void addnodeatright(node *&head,int data){
+    node *temp=new node(data);
+    head->next=temp;
+    temp->next=NULL;
+
+}
+void print(node *head){
+    node *ptr=head;
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
 int main(){
-    
+    node *n=new node(30);
+    node *head=n;
+     addnodeatright(head,123);
+    addnodeatleft(head,17);
+    addnodeatleft(head,90);
+print(head);
 }
